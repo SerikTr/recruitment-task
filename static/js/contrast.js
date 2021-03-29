7313d2e4ed7e3a5025b16e4f7be7e453557463f4
SerikTr <seriktr@gmail.com>
2021-03-29 12:37:40 +0200

    create file contrast.js for changing bg color of main page.

---
+++ static/js/contrast.js
@@ -0,0 +1,11 @@
+const btnContrast = document.getElementById('contrast__btn')
+
+document.getElementById('contrast__btn').addEventListener('click', (e) => {
+  document.querySelector('.wrapper').classList.toggle('change-bg__color')
+  document.querySelector('.header').classList.toggle('header__contrast')
+  if (btnContrast.innerText === 'CONTRAST COLOR') {
+    btnContrast.innerText = 'REMOVE CONTRAST'
+  } else {
+    btnContrast.innerText = 'CONTRAST COLOR'
+  }
+})
